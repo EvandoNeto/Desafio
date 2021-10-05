747f05562d0da7acd280e4f9be09ffbb715d9660
EvandoNeto <89491970+EvandoNeto@users.noreply.github.com>
2021-10-05 16:07:59 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,12 @@
+link = "https://github.com/pixies/devinf/releases/tag/db-base-v1.1"
+
+def repartir(link):
+    lista_link = [link.splint('/')]
+    return lista_link
+
+
+def retonar(lista_link):
+    qntd = len(lista_link)
+    versao = lista_link[qntd]
+    user = lista_link[3,4]
+    return versao,user
