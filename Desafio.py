24ed9fb1bcc98d86341ce99f7d7b7f69e47733c3
EvandoNeto <89491970+EvandoNeto@users.noreply.github.com>
2021-10-26 13:03:38 -0300

    Update Desafio.py

--- Desafio.py
+++ Desafio.py
@@ -7,7 +7,6 @@ repositorio = 'super-calculadora'
 def versao_repositorio_link(usuario, repositorio):
     url = 'https://github.com/' + usuario + '/' + repositorio + '/releases/latest'
     req = requests.get(url).url.split('https://')[1]
-    # https://github.com/pixies/calculadora-teste-py/releases/tag/v1.1
     host, usuario, repositorio, releases, tag, versao, = req.split('/')
     return {'usuario': usuario, 'repositorio': repositorio, 'versao': versao }
 
